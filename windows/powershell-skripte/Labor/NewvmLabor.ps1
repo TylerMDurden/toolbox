0f39508ef9bc15a6b88bf765918f9f039667f659
Der Micha ;-) <mi.baars@gmail.com>
2026-03-25 20:49:06 +0100

    Create NewvmLabor.ps1
    v1: Einzelne VM aus bestehender VHDX erstellen

---
+++ windows/powershell-skripte/Labor/NewvmLabor.ps1
@@ -0,0 +1,20 @@
+﻿#Erstellen einer VM mit einer bestehenden HDD
+
+# Variablen für die neue VM
+$VMName = "CL-G-Stadt"
+$VMSwitch = "G-Stadt" # z.B. "External"
+$VMPath = "C:\HyperV\Test"
+
+$NewVHDX = "C:\HyperV\VHDX\HDD-CL-G-Stadt.vhdx" # übernommen aus dem anderen Script
+
+# Neue VM erstellen und die zuvor erstellte VHDX zuweisen
+New-VM -Name $VMName -VHDPath $NewVHDX -Generation 2 -SwitchName $VMSwitch #-Path $VMPath
+
+# Arbeitsspeicher konfigurieren (optional)
+Set-VMMemory -VMName $VMName -DynamicMemoryEnabled $true -StartupBytes 2GB #-MinimumBytes 512MB -MaximumBytes 2GB
+
+# Prüfpunkte deaktivieren
+Set-VM -Name $VMName -CheckpointType Disabled
+
+# VM starten
+# Start-VM -Name $VMName
